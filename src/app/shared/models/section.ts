1fcca40664e83f0c7a46800c69d63f7958e2844a
rarmenia <rei.armenia@gmail.com>
2021-12-28 12:23:19 -0500

    section form generation

--- src/app/shared/models/section.ts
+++ src/app/shared/models/section.ts
@@ -1,7 +1,8 @@
 import {ISection} from '../interfaces/section';
 import {Statements} from './statements';
 import {QuestionGroup} from './question-group';
-import {FormControl} from '@angular/forms';
+import {FormArray, FormBuilder, FormControl, FormGroup} from '@angular/forms';
+import {NormalizedValidator} from '../types/normalized-validator';
 
 export class Section implements ISection{
 
@@ -19,4 +20,26 @@ export class Section implements ISection{
     this.retainWhenNotAsked = section.retainWhenNotAsked;
   }
 
+  public getForm(initialValue: any, fb: FormBuilder, customValidators?: Map<string, NormalizedValidator>): FormGroup | FormArray {
+    if (this.repeat) {
+      const arr = fb.array([]);
+      if (initialValue && Array.isArray(initialValue)) {
+        initialValue.forEach(value => arr.push(
+          this.formGroup(value, fb, customValidators)
+        ))
+      }
+      return arr;
+    }
+    return this.formGroup(initialValue, fb, customValidators);
+  }
+
+  public formGroup(initialValue: any, fb: FormBuilder, customValidators?: Map<string, NormalizedValidator>): FormGroup {
+    // Todo: Get Cross Field Validators on all Questions.
+    const controls = Object.entries(this.questions)
+      .reduce((prev, curr) => ({
+        ...prev,
+        [`${curr[0]}`]: curr[1].control(initialValue[curr[0]] ?? null, fb, customValidators)
+      }), {})
+    return new FormGroup(controls);
+  }
 }
